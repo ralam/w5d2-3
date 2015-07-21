2de3e57568cb5c29b9bccc9bdc7597cefe649891
App Academy Student <student@appacademy.io>
2015-07-21 15:32:32 -0700

    find and parse session cookie

--- skeleton/lib/phase4/session.rb
+++ skeleton/lib/phase4/session.rb
@@ -1,17 +1,27 @@
 require 'json'
 require 'webrick'
+require 'byebug'
 
 module Phase4
   class Session
     # find the cookie for this app
     # deserialize the cookie into a hash
     def initialize(req)
+      session_cookie = req.cookies.find { |cookie| cookie.name == "_rails_lite_app" }
+
+      if session_cookie
+        @session = JSON.parse(session_cookie.value)
+      else
+        @seession = {}
+      end
     end
 
     def [](key)
+      @session[key]
     end
 
     def []=(key, val)
+      @session[key] = value
     end
 
     # serialize the hash into json and save in a cookie
