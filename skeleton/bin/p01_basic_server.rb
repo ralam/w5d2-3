38ec1b2a9906e9efe87136329a41cc5a62610169
App Academy Student <student@appacademy.io>
2015-07-21 13:35:02 -0700

    Basic WEBrick practice

--- skeleton/bin/p01_basic_server.rb
+++ skeleton/bin/p01_basic_server.rb
@@ -1,5 +1,23 @@
 require 'webrick'
 
+server = WEBrick::HTTPServer.new(Port: 3000)
+
+server.mount_proc("/") do |request, response|
+  response.content_type = "text/text"
+  unless request.path == "/"
+    response.body = request.path
+  else
+    response.body = "root"
+  end
+
+end
+
+trap('INT') do
+  server.shutdown
+end
+
+server.start
+
 # http://www.ruby-doc.org/stdlib-2.0/libdoc/webrick/rdoc/WEBrick.html
 # http://www.ruby-doc.org/stdlib-2.0/libdoc/webrick/rdoc/WEBrick/HTTPRequest.html
 # http://www.ruby-doc.org/stdlib-2.0/libdoc/webrick/rdoc/WEBrick/HTTPResponse.html
