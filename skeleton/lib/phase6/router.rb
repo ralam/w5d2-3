f9c906c5237149bcac8a3dd939cfc382bdfa8fa8
App Academy Student <student@appacademy.io>
2015-07-22 13:29:45 -0700

    Adding basic Route and Router methods

--- skeleton/lib/phase6/router.rb
+++ skeleton/lib/phase6/router.rb
@@ -1,17 +1,26 @@
+require 'byebug'
+
 module Phase6
   class Route
     attr_reader :pattern, :http_method, :controller_class, :action_name
 
     def initialize(pattern, http_method, controller_class, action_name)
+      @pattern = pattern
+      @http_method = http_method
+      @controller_class = controller_class
+      @action_name = action_name
+
     end
 
     # checks if pattern matches path and method matches request method
     def matches?(req)
+      @http_method == req.request_method.downcase.to_sym
     end
 
     # use pattern to pull out route params (save for later?)
     # instantiate controller and call controller action
     def run(req, res)
+
     end
   end
 
@@ -19,10 +28,12 @@ module Phase6
     attr_reader :routes
 
     def initialize
+      @routes = []
     end
 
     # simply adds a new route to the list of routes
     def add_route(pattern, method, controller_class, action_name)
+      @routes << Route.new(pattern, method, controller_class, action_name)
     end
 
     # evaluate the proc in the context of the instance
