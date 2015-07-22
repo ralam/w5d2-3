6d69e31e4253d36e24443e07c536c681fd39c55a
App Academy Student <student@appacademy.io>
2015-07-22 13:55:14 -0700

    Add methods to add routes

--- skeleton/lib/phase6/router.rb
+++ skeleton/lib/phase6/router.rb
@@ -39,15 +39,18 @@ module Phase6
     # evaluate the proc in the context of the instance
     # for syntactic sugar :)
     def draw(&proc)
+      instance_eval(&proc)
     end
 
     # make each of these methods that
     # when called add route
     [:get, :post, :put, :delete].each do |http_method|
+      define_method(http_method) { add_route(@pattern, http_method, @controller_class, @action_name) }
     end
 
     # should return the route that matches this request
     def match(req)
+      @routes
     end
 
     # either throw 404 or call run on a matched route
