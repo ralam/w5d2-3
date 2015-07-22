b6c57836c61d715febbf7ad9ede321ae55784bac
App Academy Student <student@appacademy.io>
2015-07-22 15:38:38 -0700

    add Route#run, ControllerBase#invoke_action

--- skeleton/lib/phase6/router.rb
+++ skeleton/lib/phase6/router.rb
@@ -21,7 +21,10 @@ module Phase6
     # use pattern to pull out route params (save for later?)
     # instantiate controller and call controller action
     def run(req, res)
-
+      match_data = @pattern.match(req.path)
+      route_params = Hash[ match_data.names.zip( match_data.captures ) ]
+      controller = @controller_class.new(req, res, route_params)
+      controller.invoke_action(@action_name)
     end
   end
 
@@ -46,7 +49,9 @@ module Phase6
     # make each of these methods that
     # when called add route
     [:get, :post, :put, :delete].each do |http_method|
-      define_method(http_method) { add_route(@pattern, http_method, @controller_class, @action_name) }
+      define_method(http_method) do |pattern, controller_class, action_name|
+        add_route(pattern, http_method, controller_class, action_name)
+      end
     end
 
     # should return the route that matches this request
